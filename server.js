96ba59eff1d34701cb109cce0a7dc09f311ba70a
mmoh60431 <mmoh60431@gmail.com>
2026-08-25 00:06:26 +0330

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ function hashPassword(password) {
 // دیتابیس کاربران
 let users = [
     { id: 1, username: 'Test', password: hashPassword('1234'), role: 'employee', fullname: 'کاربر تست' },
-    { id: 2, username: 'mitrahajiyannezhad', password: hashPassword('mitra1368'), role: 'manager1', fullname: 'مدیر اول (میترا حاجیان‌نژاد)' },
-    { id: 3, username: 'mmp', password: hashPassword('53038386'), role: 'manager2', fullname: 'مدیر دوم' }
+    { id: 2, username: 'میترا حاجیان نژاد', password: hashPassword('mitra1368'), role: 'manager1', fullname: 'مدیر اول (میترا حاجیان‌نژاد)' },
+    { id: 3, username: 'محمد معماری پناه', password: hashPassword('53038386'), role: 'manager2', fullname: 'مدیر دوم' }
 ];
 
 let leaves = [];
